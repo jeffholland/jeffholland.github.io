24bf0dc06439c22a8a3925fd6f3fe500f602bea9
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 15:17:41 -0400

    draw smiley, also no page caching

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { drawRects, drawTriangle } from "./modules/draw.js";
+import { drawSmiley } from "./modules/draw.js";
 
 const canvas = document.getElementById("myCanvas");
 canvas.width = 150;
@@ -6,11 +6,4 @@ canvas.height = 150;
 
 const ctx = canvas.getContext("2d");
 
-const randomNumber = Math.floor(Math.random() * 2);
-
-if (randomNumber == 0) {
-    drawRects(ctx);
-} else {
-    drawTriangle(ctx);
-}
-console.log(`random number: ${randomNumber}`);
+drawSmiley(ctx);
--- modules/draw.js
+++ modules/draw.js
@@ -16,4 +16,16 @@ function drawTriangle(ctx) {
     ctx.fill();
 }
 
-export {drawRects, drawTriangle};
+function drawSmiley(ctx) {
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+    ctx.stroke();
+}
+
+export {drawRects, drawTriangle, drawSmiley};
