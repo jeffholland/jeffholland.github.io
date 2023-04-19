f4a026287c4f344faeb6fe35d176ef7982e30428
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 13:16:55 -0400

    made a color change, again to test

--- modules/draw.js
+++ modules/draw.js
@@ -1,8 +1,8 @@
 function drawRects(ctx) {
-    ctx.fillStyle = "rgb(0, 50, 200)";
+    ctx.fillStyle = "rgb(0, 50, 255)";
     ctx.fillRect(10, 10, 50, 50);
 
-    ctx.fillStyle = "rgb(0, 200, 50)";
+    ctx.fillStyle = "rgba(0, 255, 120, 0.5)";
     ctx.fillRect(30, 30, 50, 50);
 }
 
