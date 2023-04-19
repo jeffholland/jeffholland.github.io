ce5c1f4969dd8f72a56ff2732d2bacd42d8f212c
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 13:25:04 -0400

    another test change

--- modules/draw.js
+++ modules/draw.js
@@ -4,6 +4,11 @@ function drawRects(ctx) {
 
     ctx.fillStyle = "rgba(0, 255, 120, 0.5)";
     ctx.fillRect(30, 30, 50, 50);
+
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
 }
 
 export {drawRects};
