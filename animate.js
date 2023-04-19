7d97a82961036fb6d5f302dea05be0070b7d814f
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 15:52:46 -0400

    animated clock

--- animate.js
+++ animate.js
@@ -1,53 +1,104 @@
-const sun = new Image();
-const moon = new Image();
-const earth = new Image();
-function init() {
-  sun.src = "canvas_sun.png";
-  moon.src = "canvas_moon.png";
-  earth.src = "canvas_earth.png";
-  window.requestAnimationFrame(draw);
-}
-
-function draw() {
-  const ctx = document.getElementById("canvas").getContext("2d");
-
-  ctx.globalCompositeOperation = "destination-over";
-  ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-  ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
-  ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
-  ctx.save();
-  ctx.translate(150, 150);
-
-  // Earth
-  const time = new Date();
-  ctx.rotate(
-    ((2 * Math.PI) / 60) * time.getSeconds() +
-      ((2 * Math.PI) / 60000) * time.getMilliseconds()
-  );
-  ctx.translate(105, 0);
-  ctx.fillRect(0, -12, 40, 24); // Shadow
-  ctx.drawImage(earth, -12, -12);
-
-  // Moon
-  ctx.save();
-  ctx.rotate(
-    ((2 * Math.PI) / 6) * time.getSeconds() +
-      ((2 * Math.PI) / 6000) * time.getMilliseconds()
-  );
-  ctx.translate(0, 28.5);
-  ctx.drawImage(moon, -3.5, -3.5);
-  ctx.restore();
-
-  ctx.restore();
-
-  ctx.beginPath();
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-  ctx.stroke();
-
-  ctx.drawImage(sun, 0, 0, 300, 300);
-
-  window.requestAnimationFrame(draw);
-}
-
-init();
+function clock() {
+    const now = new Date();
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.save();
+    ctx.clearRect(0, 0, 150, 150);
+    ctx.translate(75, 75);
+    ctx.scale(0.4, 0.4);
+    ctx.rotate(-Math.PI / 2);
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = "white";
+    ctx.lineWidth = 8;
+    ctx.lineCap = "round";
+  
+    // Hour marks
+    ctx.save();
+    for (let i = 0; i < 12; i++) {
+      ctx.beginPath();
+      ctx.rotate(Math.PI / 6);
+      ctx.moveTo(100, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
+    ctx.restore();
+  
+    // Minute marks
+    ctx.save();
+    ctx.lineWidth = 5;
+    for (let i = 0; i < 60; i++) {
+      if (i % 5 !== 0) {
+        ctx.beginPath();
+        ctx.moveTo(117, 0);
+        ctx.lineTo(120, 0);
+        ctx.stroke();
+      }
+      ctx.rotate(Math.PI / 30);
+    }
+    ctx.restore();
+  
+    const sec = now.getSeconds();
+    const min = now.getMinutes();
+    const hr = now.getHours() % 12;
+  
+    ctx.fillStyle = "black";
+  
+    // Write image description
+    canvas.innerText = `The time is: ${hr}:${min}`;
+  
+    // Write Hours
+    ctx.save();
+    ctx.rotate(
+      (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+    );
+    ctx.lineWidth = 14;
+    ctx.beginPath();
+    ctx.moveTo(-20, 0);
+    ctx.lineTo(80, 0);
+    ctx.stroke();
+    ctx.restore();
+  
+    // Write Minutes
+    ctx.save();
+    ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(-28, 0);
+    ctx.lineTo(112, 0);
+    ctx.stroke();
+    ctx.restore();
+  
+    // Write seconds
+    ctx.save();
+    ctx.rotate((sec * Math.PI) / 30);
+    ctx.strokeStyle = "#D40000";
+    ctx.fillStyle = "#D40000";
+    ctx.lineWidth = 6;
+    ctx.beginPath();
+    ctx.moveTo(-30, 0);
+    ctx.lineTo(83, 0);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(95, 0, 10, 0, Math.PI * 2, true);
+    ctx.stroke();
+    ctx.fillStyle = "rgba(0, 0, 0, 0)";
+    ctx.arc(0, 0, 3, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.restore();
+  
+    ctx.beginPath();
+    ctx.lineWidth = 14;
+    ctx.strokeStyle = "#325FA2";
+    ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+    ctx.stroke();
+  
+    ctx.restore();
+  
+    window.requestAnimationFrame(clock);
+  }
+  
+  window.requestAnimationFrame(clock);
+  
