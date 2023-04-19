55dce01d3929e9235cfdda98fcf504e276772a43
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 15:12:55 -0400

    switchin it up

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-import { drawTriangle } from "./modules/draw.js";
+import { drawRects, drawTriangle } from "./modules/draw.js";
 
 const canvas = document.getElementById("myCanvas");
 canvas.width = 150;
 canvas.height = 150;
 
 const ctx = canvas.getContext("2d");
-drawTriangle(ctx);
+
+const randomNumber = Math.floor(Math.random() * 2);
+
+if (randomNumber == 0) {
+    drawRects(ctx);
+} else {
+    drawTriangle(ctx);
+}
