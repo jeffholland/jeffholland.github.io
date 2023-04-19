d215f583d1dc555f997765ef875071c90613e4ee
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 13:12:25 -0400

    add drawing code as module

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+import { drawRects } from "./modules/draw.js";
+
 const canvas = document.getElementById("myCanvas");
+canvas.width = 150;
+canvas.height = 150;
+
 const ctx = canvas.getContext("2d");
+drawRects(ctx);
---
+++ modules/draw.js
@@ -0,0 +1,9 @@
+function drawRects(ctx) {
+    ctx.fillStyle = "rgb(0, 50, 200)";
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = "rgb(0, 200, 50)";
+    ctx.fillRect(30, 30, 50, 50);
+}
+
+export {drawRects};
