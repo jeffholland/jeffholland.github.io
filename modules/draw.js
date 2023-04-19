cfddfba53405444817d31bb94f81ae2e67b4bef1
Jeff Holland <holland.musictech@gmail.com>
2023-04-19 15:31:21 -0400

    fixed speech bubble error

--- modules/draw.js
+++ modules/draw.js
@@ -51,9 +51,6 @@ function drawArcs(ctx) {
 }
 
 function drawSpeechBubble(ctx) {
-
-    const ctx = canvas.getContext("2d");
-
     // Quadratic curves example
     ctx.beginPath();
     ctx.moveTo(75, 25);
@@ -66,4 +63,18 @@ function drawSpeechBubble(ctx) {
     ctx.stroke();
 }
 
-export {drawRects, drawTriangle, drawSmiley, drawArcs, drawSpeechBubble};
+function drawHeart(ctx) {
+
+    // Cubic curves example
+    ctx.beginPath();
+    ctx.moveTo(75, 40);
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.fill();
+}
+
+export {drawRects, drawTriangle, drawSmiley, drawArcs, drawSpeechBubble, drawHeart};
