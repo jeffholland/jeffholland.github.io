06c7e79cc81c9db60c08b1ac795c1116ac3ead1f
Jeff Holland <holland.musictech@gmail.com>
2023-04-21 15:02:45 -0400

    store delta x and y

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-let deltaX = 1;
-let deltaY = 0;
 
-document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "ArrowLeft":
-            deltaX = -1;
-            deltaY = 0;
-            break;
-        case "ArrowRight":
-            deltaX = 1;
-            deltaY = 0;
-            break;
-        case "ArrowUp":
-            deltaY = -1;
-            deltaX = 0;
-            break;
-        case "ArrowDown":
-            deltaY = 1;
-            deltaX = 0;
-            break;
-    }
-})
 
 const canvasWidth = 600;
 const canvasHeight = 600;
@@ -37,18 +15,50 @@ const rectWidth = 10;
 const rectHeight = 10;
 let rectX;
 let rectY;
+let deltaX;
+let deltaY;
+
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowLeft":
+            deltaX = -1;
+            deltaY = 0;
+            localStorage.setItem("deltaX", deltaX);
+            localStorage.setItem("deltaY", deltaY);
+            break;
+        case "ArrowRight":
+            deltaX = 1;
+            deltaY = 0;
+            localStorage.setItem("deltaX", deltaX);
+            localStorage.setItem("deltaY", deltaY);
+            break;
+        case "ArrowUp":
+            deltaY = -1;
+            deltaX = 0;
+            localStorage.setItem("deltaX", deltaX);
+            localStorage.setItem("deltaY", deltaY);
+            break;
+        case "ArrowDown":
+            deltaY = 1;
+            deltaX = 0;
+            localStorage.setItem("deltaX", deltaX);
+            localStorage.setItem("deltaY", deltaY);
+            break;
+    }
+})
 
 if (localStorage.getItem("xpos")) {
-    console.log("Hi");
     rectX = parseInt(localStorage.getItem("xpos"));
     rectY = parseInt(localStorage.getItem("ypos"));
-    console.log(`getting rectangle at (${rectX}, ${rectY})`);
+    deltaX = parseInt(localStorage.getItem("deltaX"));
+    deltaY = parseInt(localStorage.getItem("deltaY"));
 } else {
     rectX = 0;
     rectY = 0;
+    deltaX = 1;
+    deltaY = 0;
 }
 
-let runCount = 0;
 function drawRect(x, y) {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ctx.fillRect(x, y, rectWidth, rectHeight);
@@ -60,19 +70,14 @@ function main() {
     drawRect(rectX, rectY);
     rectX += deltaX;
     rectY += deltaY;
-    if (runCount < 5) {
-        console.log(`drew rectangle at (${rectX}, ${rectY})`);
-        runCount++;
-    }
+
     if (rectX >= canvasWidth - rectWidth) {
-        console.log(`why's this called? rectX: ${rectX} canvasWidth: ${canvasWidth} `);
         rectX = 0;
     }
     if (rectX < 0) {
         rectX = canvasWidth - rectWidth;
     }
     if (rectY >= canvasHeight - rectHeight) {
-        console.log(`why's this called? rectY: ${rectY} canvasHeight: ${canvasHeight} `);
         rectY = 0;
     }
     if (rectY < 0) {
